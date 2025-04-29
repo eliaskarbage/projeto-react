56fab4ea3e82e61c9ba998333bbab7d68cf0a114
Seu nome aqui <seuemail@email.com>
2025-04-29 19:32:59 -0300

    alterações no componente de saudação

--- aula1-react/src/components/Saudacao/index.jsx
+++ aula1-react/src/components/Saudacao/index.jsx
@@ -1,7 +1,7 @@
 import './style.css';
 
 function Saudacao({nome, sobrenome}) {
-    return <p className='titulo'>Olá, {nome} {sobrenome}! Seja bem vindo!</p>
+    return <p className='titulo'>Olá, {nome} {sobrenome}! Seja bem vindo ao nosso site!</p>
 }
 
 export default Saudacao;
